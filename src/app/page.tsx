6aabf5f3db46a9cba3a038c8f378fc953732ec0f
juvidev <jvivas@correo.iue.edu.co>
2024-04-18 21:06:21 -0500

    Add size filters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,6 +43,16 @@ const COLOR_FILTERS = {
   ] as const,
 };
 
+const SIZE_FILTERS = {
+  id: "size",
+  name: "Size",
+  options: [
+    { value: "S", label: "S" },
+    { value: "M", label: "M" },
+    { value: "L", label: "L" },
+  ],
+} as const;
+
 const SUBCATEGORIES = [
   { name: "T-Shirts", selected: true, href: "#" },
   { name: "Hoodies", selected: false, href: "#" },
@@ -185,6 +195,40 @@ export default function Home() {
                         />
                         <label
                           htmlFor={`color-${optionIdx}`}
+                          className="ml-3 text-sm text-gray-600"
+                        >
+                          {option.label}
+                        </label>
+                      </li>
+                    ))}
+                  </ul>
+                </AccordionContent>
+              </AccordionItem>
+
+              {/* Size filters */}
+              <AccordionItem value="size">
+                <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-500">
+                  <span className="font-medium text-gray-900">Size</span>
+                </AccordionTrigger>
+
+                <AccordionContent className="pt-6 animate-none">
+                  <ul className="space-y-4">
+                    {SIZE_FILTERS.options.map((option, optionIdx) => (
+                      <li key={option.value} className="flex items-center">
+                        <input
+                          type="checkbox"
+                          id={`size-${optionIdx}`}
+                          onChange={() => {
+                            applyArrayFilter({
+                              category: "size",
+                              value: option.value,
+                            });
+                          }}
+                          checked={filter.size.includes(option.value)}
+                          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                        />
+                        <label
+                          htmlFor={`size-${optionIdx}`}
                           className="ml-3 text-sm text-gray-600"
                         >
                           {option.label}
