ad2c7fbe400e424028cc4da81e4617337ef84298
juvidev <jvivas@correo.iue.edu.co>
2024-04-18 21:00:41 -0500

    Add color filter & product validator

--- src/components/Products/Product.tsx
+++ src/components/Products/Product.tsx
@@ -4,11 +4,13 @@ const Product = ({ product }: { product: TProduct }) => {
   return (
     <div className="group relative">
       <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-        <img
-          src={product.imageId}
-          alt="product image"
-          className="h-full w-full object-cover object-center"
-        />
+        <picture>
+          <img
+            src={product.imageId}
+            alt="product image"
+            className="h-full w-full object-cover object-center"
+          />
+        </picture>
       </div>
       <div className="mt-4 flex justify-between">
         <div>
